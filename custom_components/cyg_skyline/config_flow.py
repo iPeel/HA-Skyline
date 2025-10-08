67c56ece2aa03c584ba8e28c70dec13952c35d01
iPeel <49528212+iPeel@users.noreply.github.com>
2025-10-08 17:41:46 +0100

    Update config_flow.py

--- custom_components/cyg_skyline/config_flow.py
+++ custom_components/cyg_skyline/config_flow.py
@@ -148,7 +148,7 @@ class OptionsFlowHandler(OptionsFlow):
 
     def __init__(self, config_entry: ConfigEntry) -> None:
         """Initialize options flow."""
-        self.config_entry = config_entry
+        #self.config_entry = config_entry
 
     async def async_step_init(
         self, user_input: dict[str, Any] | None = None
